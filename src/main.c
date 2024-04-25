a16590efb44f0dad5b58387d8e5ada8c5cf0fe9a
LaughableHammer <kushaagra.kesarwani@gmail.com>
2024-04-25 21:14:44 +1000

    Organised code, added input error checking and made error outputs more streamlined

--- src/main.c
+++ src/main.c
@@ -3,17 +3,30 @@
 #include <unistd.h>
 #include <string.h>
 
-enum hash_type {LM, NTLM, MD2, MD4, MD5, SHA1, SHA224, SHA256, SHA384, SHA512};
-const char *hash_names[] = {"LM", "NTLM", "MD2", "MD4", "MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512"};
+#define INVALID_HASH_FILE -1
+#define INVALID_WORDLIST_FILE -2
+#define INVALID_HASH_TYPE -3
+#define INVALID_INPUT -4
+
+#define NUM_HASH_TYPES 10
+
+enum hash_type {LM = 1, NTLM, MD2, MD4, MD5, SHA1, SHA224, SHA256, SHA384, SHA512};
+const char *hash_names[] = {"null", "LM", "NTLM", "MD2", "MD4", "MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512"};
+struct input {
+    char *hash_path;
+    char *wordlist_path;
+    enum hash_type hash_type;
+};
 
 void print_help();
+void end_program(int error_code);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    struct input input;
     int option;
-    char *hash_path = "";
-    char *wordlist_path = "";
-    enum hash_type hash_type;
+    
+    FILE *hash_file;
+    FILE *wordlist_file;
 
     if (argc == 1) {
         print_help();
@@ -22,36 +35,49 @@ int main(int argc, char *argv[])
 
     while ((option = getopt(argc, argv, ":w:i:h:")) != EOF) {
         switch (option) {
-        case '?':
-            printf("Unknown option: %c\n", optopt);
-            print_help();
-            break;
-        case 'w':
-            wordlist_path = optarg;
-            printf("Wordlist: %s\n", wordlist_path);
-            break;
-        case 'i':
-            hash_path = optarg;
-            printf("Hash: %s\n", hash_path);
-            break;
-        case 'h':
-            hash_type = atoi(optarg);
-            printf("Hash type: %s\n", hash_names[hash_type]);
-            break;
-        case 'f':
-            printf("filename : %s\n", optarg);
-            break;
-        case ':':
-            printf("Option -%c requires an argument\n", optopt);
-            print_help();
-            break;
+            case '?':
+                printf("Unknown option: %c\n", optopt);
+                print_help();
+                break;
+            case 'w':
+                input.wordlist_path = optarg;
+                printf("Wordlist: %s\n", input.wordlist_path);
+                break;
+            case 'i':
+                input.hash_path = optarg;
+                printf("Hash: %s\n", input.hash_path);
+                break;
+            case 'h':
+                input.hash_type = atoi(optarg);
+                printf("Hash type: %s\n", hash_names[input.hash_type]);
+                break;
+            case ':':
+                printf("Option -%c requires an argument\n", optopt);
+                print_help();
+                break;
         }
     }
 
-    for(; optind < argc; optind++){
-    	printf("extra arguments: %s\n", argv[optind]);
+    for(; optind < argc; optind++) {
+        end_program(INVALID_INPUT);
     }
 
+    hash_file = fopen(input.hash_path, "r");
+    wordlist_file = fopen(input.wordlist_path, "r");
+
+    if (hash_file == NULL) {
+        end_program(INVALID_HASH_FILE);
+    } else if (wordlist_file == NULL) {
+        end_program(INVALID_WORDLIST_FILE);
+    } else if (input.hash_type < 1 || input.hash_type > NUM_HASH_TYPES) {
+        end_program(INVALID_HASH_TYPE);
+    } else {
+        bruteforce(hash_file, wordlist_file, input.hash_type);
+    }
+
+
+    fclose(hash_file);
+    fclose(wordlist_file);
     return 0;
 }
 
@@ -63,4 +89,30 @@ void print_help() {
     printf("  -h <hashtype>    Specify hash type (0-9)\n");
     printf("  -help            Display this help message\n");
     exit(0);
+}
+
+void end_program(int error_code) {
+    switch (error_code) {
+        case -1:
+            printf("Invalid hash file\n");
+            break;
+        case -2:
+            printf("Invalid wordlist file\n");
+            break;
+        case -3:
+            printf("Invalid hash type\n");
+            break;
+        case -4:
+            printf("Invalid command line arguments provided\n");
+            break;
+    }
+
+    print_help();
+    exit(0);
+}
+
+void bruteforce(FILE *hash_file, FILE *wordlist_file, int hash_type) {
+    // function implmentation
+
+    return;
 }
